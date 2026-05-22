04afacb5aee87fdc1dd648b0e5837e3ccb9cb20b
asti <bondarartem324@gmail.com>
2026-05-22 20:55:40 +0300

    Добавлен класс BondarClass в проект
    В файл `BondarClass.cs` добавлены директивы `using` для подключения необходимых пространств имен.
    Создан новый класс `BondarClass` в пространстве имен `TeamProject.Models` с двумя свойствами: `Id` (int) и `Name` (string).

---
+++ TeamProject/Models/BondarClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject.Models;
+
+public class BondarClass
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
