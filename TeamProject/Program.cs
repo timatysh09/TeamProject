122533fb1b74495ed6788586deae6e2073be21e5
Artem Bondar <bondarartem324@gmail.com>
2026-05-22 21:20:03 +0300

    Update Program.cs
    add Console.WriteLine("Artem Bondar");

--- TeamProject/Program.cs
+++ TeamProject/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Tymofii Tyschyk");
 Console.WriteLine("Pavlo Los");
-Console.WriteLine("Artem");
+Console.WriteLine("Artem Bondar");
 Console.WriteLine("Vasyl Kovalov"); // Teacher
