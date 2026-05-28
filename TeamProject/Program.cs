49e8c0314bb2da00e031506b68578c9aa1a70e6a
Vlad <vladkalina2009@gmail.com>
2026-05-28 21:16:34 +0300

    added class

--- TeamProject/Program.cs
+++ TeamProject/Program.cs
@@ -13,3 +13,6 @@ Console.WriteLine("Somyk Yurii");
 
 IProjectPart projectPart = new CurrentDateTimeProjectPart();
 projectPart.Execute();
+
+IProjectPart kalinichenkoPart = new Kalinichenko();
+kalinichenkoPart.Execute();
