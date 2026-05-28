c9c09efe37ed232d2349684f4a655927a37b9f90
Tymofii Tyshchyk <timatimych09@gmail.com>
2026-05-28 18:39:32 +0300

    Тищик Тимофій додав клас.

---
+++ TeamProject/Models/Tyshchyk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject.Models
+{
+    internal class Tyshchyk
+    {
+        public int ammo {  get; set; }
+        public int life { get; set; }
+        public int score { get; set; }
+        public string name { get; set; }
+    }
+}
