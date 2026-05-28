ae89e364b3d8bb821f12e71e1da7acb4420e08d0
aboba <sasauperenko105@gmail.com>
2026-05-28 18:14:45 +0000

    Refactor Program.cs to include MyInfoProjectPart execution

--- TeamProject/Program.cs
+++ TeamProject/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TeamProject;
+using TeamProject.Interfaces;
+using TeamProject.Models;
 Console.WriteLine("Uperenko Olexander");
+IProjectPart myInfoProjectPart = new MyInfoProjectPart();
+myInfoProjectPart.Execute();
