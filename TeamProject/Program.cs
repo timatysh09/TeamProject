27bd7416b97075710d76e902e0f813306e999083
aboba <sasauperenko105@gmail.com>
2026-05-28 16:51:35 +0000

    deleted:    TeamProject/Models/BondarClass.cs 	deleted:    TeamProject/Models/KovalovClass.cs 	deleted:    TeamProject/Models/TestClass.cs 	modified:   TeamProject/Program.cs

--- TeamProject/Program.cs
+++ TeamProject/Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Tymofii Tyschyk");
-Console.WriteLine("Pavlo Los");
-Console.WriteLine("Artem Bondar");
-Console.WriteLine("Vasyl Kovalov"); // Teacher
 Console.WriteLine("Uperenko Olexander");
