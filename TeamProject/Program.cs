3aaefddf1e2cba0121468374b84891aaf9046f71
Vlad <vladkalina2009@gmail.com>
2026-05-29 20:12:26 +0300

    злився з усима гілками

--- TeamProject/Program.cs
+++ TeamProject/Program.cs
@@ -30,10 +30,5 @@ losProjectPart.Execute();
 IProjectPart projectPart1 = new Tyshchyk();
 projectPart1.Execute();
 
-// See https://aka.ms/new-console-template for more information
-using TeamProject;
-using TeamProject.Interfaces;
-using TeamProject.Models;
-Console.WriteLine("Uperenko Olexander");
 IProjectPart myInfoProjectPart = new MyInfoProjectPart();
 myInfoProjectPart.Execute();
