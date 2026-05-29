5190a2f666de7918446156b3ff6663a1b5212d52
Vasyl Kovalov <raid3r81@gmail.com>
2026-05-29 19:08:40 +0300

    Add Kovalov part

--- TeamProject/Program.cs
+++ TeamProject/Program.cs
@@ -13,3 +13,6 @@ Console.WriteLine("Somyk Yurii");
 
 IProjectPart projectPart = new CurrentDateTimeProjectPart();
 projectPart.Execute();
+
+var kovalovPart = new KovalovProjectPart();
+kovalovPart.Execute();
